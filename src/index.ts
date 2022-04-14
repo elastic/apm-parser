e9d4afad1656f39aaf1ebbb8979ee37bf2d4debc
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-04-14 10:31:19 +0200

    temp solution

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import { ApmClient, Environment } from './ApmClient';
 import _ from 'lodash';
 import * as sanitize from "./sanitize";
 
+import {initClient} from './es_client'
+
 type CLIParams = {
   dir: string;
   type: string;
@@ -55,6 +57,43 @@ const enrichTrace = async (transactionName: string, transactionType: string, ser
 
 const apmParser = async ({ param, client }: CLIParams) => {
   const api = new ApmClient(client);
+  const authOptions = {
+    node: 'https://apm-7-17.es.us-central1.gcp.cloud.es.io:9243',
+    username: process.env.ES_USER,
+    password: process.env.ES_PSWD,
+  }
+  const esClient = initClient(authOptions);
+  const hits = await esClient.getTransactions(process.env.JOB_ID); // '2022-04-13T11:31:18.192Z'
+  // with login: "local-a1715652-65b8-4125-8a42-ddbe99f9d63f"
+  // without login: local-b47b6b73-067b-44af-a9ff-4a7fabda271d
+  if (hits && hits.length > 0) {
+    const data = hits.map(hit => hit._source).map(hit => {
+      return  {
+        // processor: hit.processor,
+        // labels: hit.labels,
+        traceId : hit.trace.id,
+        timestamp: hit["@timestamp"],
+        // environment: hit.environment,
+        request: {
+          url: { path: hit.url.path },
+          // headers: hit.http.request.headers,
+          method: hit.http.request.method,
+          body: hit.http.request.body ? JSON.parse(hit.http.request.body.original) : '',
+        },
+        // response: {statusCode: hit.http.response.status_code},
+        transaction: {
+          id: hit.transaction.id,
+          name: hit.transaction.name,
+          type: hit.transaction.type,
+        }
+      }
+    });
+
+    data.forEach(i => console.log(JSON.stringify(i)));
+    console.log(`Found ${hits.length} hits`);
+    await writeFile('./transactions.json',JSON.stringify(data, null, 2), 'utf8');
+  }
+  return;
   console.log(`Querying APM with condition (${param.kuery}) starting from ${param.start} and ending at ${param.end}`);
 
   const transactionGroupMainStatistics = await Promise.all(param.transactionType.map(transactionType => api.getTransactionGroupMainStatistics(transactionType === 'http-request' ? 'kibana-frontend' : 'kibana', {
